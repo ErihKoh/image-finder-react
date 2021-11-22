772a891bc2d8984ef7bd301ccf8f79732190f4ef
Erih Koh <barsuk0831@gmail.com>
2021-11-22 12:01:38 +0200

    Updates

---
+++ precache-manifest.d1ca30851ea8c7fd0327a35b1da19198.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d06e60710533537e97afaeb4c8098da5",
+    "url": "/image-finder-react/index.html"
+  },
+  {
+    "revision": "9dd9d9dc4f0bedb7f0b0",
+    "url": "/image-finder-react/static/css/2.4d30024f.chunk.css"
+  },
+  {
+    "revision": "9b9d68efa52f305b30db",
+    "url": "/image-finder-react/static/css/main.c5eda497.chunk.css"
+  },
+  {
+    "revision": "9dd9d9dc4f0bedb7f0b0",
+    "url": "/image-finder-react/static/js/2.7d188e41.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/image-finder-react/static/js/2.7d188e41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8fbae3c0c866902143c",
+    "url": "/image-finder-react/static/js/3.91f0ba59.chunk.js"
+  },
+  {
+    "revision": "9b9d68efa52f305b30db",
+    "url": "/image-finder-react/static/js/main.4d1ddba8.chunk.js"
+  },
+  {
+    "revision": "6a79d138f7daed108cf5",
+    "url": "/image-finder-react/static/js/runtime-main.b0a5ccb0.js"
+  }
+]);
